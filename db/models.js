3d7f49f001799f586548257f83397d8c25a1e13c
Andrew Munoz <ajmunoz411@gmail.com>
2021-06-28 16:57:43 -0700

    getProductStyles query and functionality done

--- db/models.js
+++ db/models.js
@@ -66,31 +66,58 @@ const models = {
     // );
     console.log('hello from product styles');
     const { product_id } = req.params;
+    // const queryStr2 = (`
+    //   SELECT
+    //     array_to_json(array_agg(row_to_json(t)))
+    //   FROM (
+    //     select id as style_id, name, original_price, sale_price, "default" as "default?",
+    //     (
+    //       select array_to_json(array_agg(row_to_json(d)))
+    //       from (
+    //         select thumbnail_url, url
+    //         from products.photos
+    //         where style_id=products.styles.id
+    //       ) d
+    //     ) as photos
+    //     from products.styles
+    //     where product_id=${product_id}
+    //   ) t
+    // `);
     const queryStr2 = (`
-      SELECT
-        array_to_json(array_agg(row_to_json(t)))
+      SELECT JSON_BUILD_OBJECT(
+        'product_id', ${product_id},
+        'results', json_agg(row_to_json(t))
+      ) AS stylesObj
       FROM (
-        select id as style_id, name, original_price, sale_price, "default" as "default?",
+        SELECT id AS style_id, name, original_price, sale_price, "default" AS "default?",
+          (
+            SELECT ARRAY_TO_JSON(ARRAY_AGG(ROW_TO_JSON(d)))
+            FROM (
+              SELECT thumbnail_url, url
+              FROM products.photos
+              WHERE style_id=products.styles.id
+            ) d
+        ) AS photos,
         (
-          select array_to_json(array_agg(row_to_json(d)))
-          from (
-            select thumbnail_url, url
-            from products.photos
-            where style_id=products.styles.id
-          ) d
-        ) as photos
-        from products.styles
-        where product_id=${product_id}
-      ) t
-    `);
+          SELECT JSON_OBJECT_AGG(
+            products.skus.id,
+            JSON_BUILD_OBJECT(
+              'size', products.skus.size,
+              'quantity', products.skus.quantity
+          ))
+        FROM products.skus
+        WHERE style_id=products.styles.id
+      ) AS skus
+      FROM products.styles
+      WHERE product_id=${product_id}
+      ) t`);
     db.query(queryStr2, (err, data) => {
       if (err) {
         cb(err);
       } else {
-        cb(null, data.rows);
+        cb(null, data.rows[0].stylesobj);
       }
     });
-    // returns array of style objects
   },
 
   getRelatedProducts: (req, cb) => {
