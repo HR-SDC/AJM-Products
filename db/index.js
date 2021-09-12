74b1271770123ef732a33e65062820252a6edd60
Andrew Munoz <ajmunoz411@gmail.com>
2021-09-11 20:20:13 -0700

    db Index updated

--- db/index.js
+++ db/index.js
@@ -3,13 +3,22 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'SDCProducts',
+  database: 'SDCProducts2',
   password: 'p0stgr3s',
   port: 5432,
+  allowExitOnIdle: true,
 });
 
-pool.connect();
+module.exports = pool;
 
-module.exports = {
-  query: (text, params, callback) => pool.query(text, params, callback),
-};
+// pool.connect((err) => {
+//   if (err) {
+//     console.log('err connecting to db', err.stack);
+//   } else {
+//     console.log('connected to psql db');
+//   }
+// });
+
+// module.exports = {
+//   query: (text, params, callback) => pool.query(text, params, callback),
+// };
