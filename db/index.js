7f716291f68c3345502560773768cc0af59844f8
Andrew Munoz <ajmunoz411@gmail.com>
2021-09-11 20:24:43 -0700

    Comment removed from db index, script.js moved to archived

--- db/index.js
+++ db/index.js
@@ -10,15 +10,3 @@ const pool = new Pool({
 });
 
 module.exports = pool;
-
-// pool.connect((err) => {
-//   if (err) {
-//     console.log('err connecting to db', err.stack);
-//   } else {
-//     console.log('connected to psql db');
-//   }
-// });
-
-// module.exports = {
-//   query: (text, params, callback) => pool.query(text, params, callback),
-// };
